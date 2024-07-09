3b2f4e51c687f717100c04d2c1177e9f1adaa0cb
dmltdev <hone.domitoro@gmail.com>
2024-07-09 15:17:18 +0300

    chore: filter setup.sh scripts to use system-wide

--- setup.sh
+++ setup.sh
@@ -2,11 +2,7 @@
 # Source all .sh files except those that are listed in the first scenario
 for file in ~/shell-scripts/*.sh; do
   case "$file" in
-    *setup.sh|*install.sh|*ensure_shebang.sh)
-      # Skip these files
-      continue
-      ;;
-    *)
+    *sht.sh|ns.sh|lt.sh|v.sh)
       source "$file"
       ;;
   esac
