7e1283420927d8dfcf52058c3fc5e910793546d2
dmltdev <hone.domitoro@gmail.com>
2024-08-10 22:17:14 +0300

    feat(gitlines.sh): count only same git user lines

--- gitlines.sh
+++ gitlines.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Script to observe today's repository activity (added and removed lines count)
+# Script to observe today's repository activity (added and removed lines count) by the current user
 gitlines() {
   today=$(date +"%Y-%m-%d")
+  
+  author=$(git config user.name)
 
-  git log --since="$today 00:00" --until="$today 23:59" --pretty=tformat: --numstat | awk '
+  git log --author="$author" --since="$today 00:00" --until="$today 23:59" --pretty=tformat: --numstat | awk '
   { 
     added += $1; 
     removed += $2 
   } 
   END { 
-    printf "Lines Added Today: %s\nLines Removed Today: %s\n", added ? added : 0, removed ? removed : 0 
+    printf "Lines Added Today by %s: %s\nLines Removed Today by %s: %s\n", "'$author'", added ? added : 0, "'$author'", removed ? removed : 0 
   }'
 }
-
